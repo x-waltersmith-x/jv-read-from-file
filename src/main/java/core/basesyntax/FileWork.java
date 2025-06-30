45285840f49b6b90b437925d0995de071b739ba6
Volodymyr Kovalov <vkovalov@Volodymyrs-Mac-mini.local>
2025-06-30 13:18:03 +0200

    Try to complete task

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,40 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        List<String> wWords = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] words = line.split("[\\s\\p{Punct}]+");
+
+                for (String word : words) {
+                    if (!word.isEmpty() && (word.toLowerCase().startsWith("w"))) {
+                        String cleanWord = word.replaceAll("\\p{Punct}", "").toLowerCase();
+
+                        if (cleanWord.startsWith("w")) {
+                            wWords.add(cleanWord);
+                        }
+                    }
+                }
+
+            }
+        } catch (IOException e) {
+            return new String[0];
+        }
+
+        String[] result = wWords.toArray(new String[0]);
+        Arrays.sort(result);
+        return result;
     }
+
 }
